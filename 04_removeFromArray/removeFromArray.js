afd77f0034d25093a9bbb385b95dfa81f8198070
RGB-2025 <205908174+RGB-2025@users.noreply.github.com>
2025-04-08 18:57:21 +0800

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array = [], ...remove) {
+    return array.filter((i) => !remove.includes(i));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
