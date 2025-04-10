0e02abd92a2d7e6a7e4ca19d5fd48afc7bb5f1a6
RGB-2025 <205908174+RGB-2025@users.noreply.github.com>
2025-04-10 19:07:54 +0800

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,4 @@
-const fibonacci = function() {
-
-};
+const fibonacci=function(num){num=Number(num);return num>=0?num==0||num==1?num:fibonacci(num-1)+fibonacci(num-2):'OOPS'};
 
 // Do not edit below this line
 module.exports = fibonacci;
