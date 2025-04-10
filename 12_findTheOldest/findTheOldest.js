bc5212d261ac8323c4e345372bb9e263302b0d40
RGB-2025 <205908174+RGB-2025@users.noreply.github.com>
2025-04-10 19:51:39 +0800

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => 
+        (oldest.hasOwnProperty('yearOfDeath') 
+            ? oldest.yearOfDeath - oldest.yearOfBirth 
+            : new Date().getFullYear() - oldest.yearOfBirth) 
+        > (person.hasOwnProperty('yearOfDeath') 
+            ? person.yearOfDeath - person.yearOfBirth 
+            : new Date().getFullYear() - person.yearOfBirth) 
+        ? oldest : person
+    );
 };
 
 // Do not edit below this line
