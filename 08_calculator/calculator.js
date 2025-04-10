551eaaa87ce0ea92aa9019670650115a54232cfc
RGB-2025 <205908174+RGB-2025@users.noreply.github.com>
2025-04-10 18:26:42 +0800

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total*current);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	return x == 0 || x == 1 ? 1 : multiply(Array.from({length: x}, (_, i) => i+1));
 };
 
 // Do not edit below this line
