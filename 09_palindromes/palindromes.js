c49d713115af422704fe324ea5c5c5c017563cd3
RGB-2025 <205908174+RGB-2025@users.noreply.github.com>
2025-04-10 18:46:51 +0800

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,4 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {return string.replace(/\W|_/g, '').toLowerCase().split('').reverse().join('') === string.replace(/\W|_/g, '').toLowerCase()};
 
 // Do not edit below this line
 module.exports = palindromes;
